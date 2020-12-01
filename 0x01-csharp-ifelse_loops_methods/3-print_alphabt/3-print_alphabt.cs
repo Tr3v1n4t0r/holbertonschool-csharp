68fb1bbaf409f7590e9b373b7dc22ada82928324
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-01 15:47:02 -0800

    t3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char i = 'a'; i <= 'z'; i++)
+            if (i != 'q' && i != 'e')
+                Console.Write(i);
+    }
+}
