aa896788462d247e593b3e7cfd25a22a6c24f7ad
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-01 18:03:00 -0800

    t12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+                Console.Write("Fizz");
+            if (i % 5 == 0)
+                Console.Write("Buzz");
+            if (i % 3 != 0 && i % 5 != 0)
+                Console.Write("{0}", i);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
