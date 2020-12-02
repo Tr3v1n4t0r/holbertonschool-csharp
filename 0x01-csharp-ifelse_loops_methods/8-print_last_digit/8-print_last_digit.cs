28abb8806c7dc11741d46a73ae75df337dac438d
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-01 16:16:43 -0800

    t8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        number = Math.Abs(number % 10);
+        Console.Write(number);
+        return number;
+    }
+}
