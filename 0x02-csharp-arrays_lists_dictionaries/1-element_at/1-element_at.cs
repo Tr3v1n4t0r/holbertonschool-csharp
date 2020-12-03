e559bb8956877e5fcbff77133279c1231c32bd35
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-02 22:05:17 -0800

    t1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index >= array.Length || index < 0)
+        {
+            Console.Write("Index out of range");
+            return -1;
+        }
+        return array[index];
+    }
+}
