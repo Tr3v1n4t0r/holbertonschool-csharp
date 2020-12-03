b3c11df6e8ee05c391483a9f5ffe240e9f6fc855
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 12:16:31 -0800

    t3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        bool found = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, found);
+        if (found)
+        {
+            string val;
+            do {
+                val = aStack.Pop();
+            }
+            while (val != search);
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
