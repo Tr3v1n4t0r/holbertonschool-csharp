03467eb990af78363d0e578518f7fc66cf04d75a
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 15:42:25 -0800

    t6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        foreach (int val in myLList)
+            count++;
+        return count;
+    }
+}
