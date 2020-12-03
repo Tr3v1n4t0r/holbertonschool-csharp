2e48181fe69785843752c6de59f149411da31fd8
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-02 22:55:01 -0800

    t6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> ret = new List<bool>();
+
+        foreach (int val in myList)
+            ret.Add(val % 2 == 0);
+        return ret;
+    }
+}
