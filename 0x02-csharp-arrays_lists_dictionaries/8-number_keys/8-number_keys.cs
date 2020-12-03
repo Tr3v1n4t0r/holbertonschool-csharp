715814d6508716b8cc0efdc450562409c2ce4425
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-02 23:18:59 -0800

    t8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int size = 0;
+        foreach (string key in myDict.Keys)
+            size++;
+        return size;
+    }
+}
