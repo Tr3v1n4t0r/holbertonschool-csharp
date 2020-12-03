09f098158dabb81f0ae1fba441e1bf7a75c3b84e
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 14:32:47 -0800

    t4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        bool found = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, found);
+        if (found)
+        {
+            string val;
+            do {
+                val = aQueue.Dequeue();
+            }
+            while (val != search);
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
