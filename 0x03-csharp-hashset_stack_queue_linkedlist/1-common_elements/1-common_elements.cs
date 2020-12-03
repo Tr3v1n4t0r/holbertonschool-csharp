38453477b39384551a5746a66d4b0cc7f50075c3
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 12:04:39 -0800

    t1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common =  new List<int>();
+        foreach (int val in list1)
+        {
+            if (list2.Contains(val))
+                common.Add(val);
+        }
+        return common;
+    }
+}
