8bec3f3bc8e2d766d8d73f598777965ff22de143
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 23:53:26 -0800

    unneeded brackets

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -6,9 +6,7 @@ class LList
     public static int Pop(LinkedList<int> myLList)
     {
         if (myLList.Count == 0)
-        {
             return 0;
-        }
         int value = myLList.First.Value;
         myLList.RemoveFirst();
         return value;
