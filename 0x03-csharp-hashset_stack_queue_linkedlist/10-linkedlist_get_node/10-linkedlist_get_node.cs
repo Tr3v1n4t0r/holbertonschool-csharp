de20edbda3a13b0ef3fc2bbc4ac40a1c6edeecf0
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-03 23:54:06 -0800

    t10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (int val in myLList)
+        {
+            if (index == n)
+                return val;
+            index++;
+        }
+        return 0;
+    }
+}
