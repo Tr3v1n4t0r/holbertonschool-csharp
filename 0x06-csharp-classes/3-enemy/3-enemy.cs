4ce467347f9772eefc8108dafcb3a937aafa5fe2
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-15 21:06:51 -0800

    t3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>A zombie class.</summary>
+    public class Zombie
+    {
+        /// <summary>Zombie health.</summary>
+        public int health;
+        /// <summary>Zombie Constructor.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Zombie Constructor.</summary>
+        /// <param name="value">Health</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            this.health = value;
+        }
+        /// <summary>Gets Zombie health.</summary>
+        /// <returns>Zombie health</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
