c2b6f7bab9e0ce30d882faef8a334f19881328f7
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-16 15:00:55 -0800

    t0

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -9,10 +9,15 @@ namespace MyMath.Tests
         {
         }
 
-        [Test]
+        [TestCase(1, 0)]
+        [TestCase(2, 6)]
+        [TestCase(-6, -8)]
+        [TestCase(-14, 11)]
+        [TestCase(0, 0)]
         public void Test1()
         {
-            Assert.Pass();
+            int result = MyMath.Operations.Add(a, b);
+            Assert.AreEqual(result, a + b);
         }
     }
 }
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,7 +2,16 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>Basic math operations.</summary>
+    public class Operations
     {
+        /// <summary>Adds the two parameters together.</summary>
+        /// <param name="a">The first number.</param>
+        /// <param name="b">The second number.</param>
+        /// <returns>The sum of a and b.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
