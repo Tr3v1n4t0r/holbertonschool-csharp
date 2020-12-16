54249f2b6e660562dfc8f143953362715b7ef6d2
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-15 20:56:10 -0800

    t1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>A zombie class.</summary>
+    public class Zombie
+    {
+        /// <summary>Zombie health.</summary>
+        public int health;
+        /// <summary>Zombie Constructor.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
