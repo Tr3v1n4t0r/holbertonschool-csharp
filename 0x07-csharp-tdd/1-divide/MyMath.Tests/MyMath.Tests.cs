2da97c2aeaea820560292fd7d941aab4fff48819
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-16 16:29:05 -0800

    t1

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Positive_by_Positive()
+        {
+            int[,] input = new int[,] {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            int [,] output = MyMath.Matrix.Divide(input, 2);
+            int[,] expected = new int[,] {
+                {0, 1, 1},
+                {2, 2, 3},
+                {3, 4, 5}
+            };
+            Assert.AreEqual(output, expected);
+        }
+
+        [Test]
+        public void Positive_by_Zero()
+        {
+            int[,] input = new int[,] {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            int [,] output = MyMath.Matrix.Divide(input, 0);
+            Assert.IsNull(output);
+        }
+
+        [Test]
+        public void Null()
+        {
+            int [,] output = MyMath.Matrix.Divide(null, 3);
+            Assert.IsNull(output);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Class for matrix math functions.</summary>
+    public class Matrix
+    {
+        /// <summary>Divides each element in a matrix by a given number.</number>
+        /// <param name="matrix">A matrix of integers.</param>
+        /// <param name="num">The number to divide the matrix elements by.</param>
+        /// <returns>A new matrix with the results.</returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            if (matrix == null)
+                return null;
+            
+            int[,] newMatrix = (int[,])matrix.Clone();
+            for (int i = 0; i < newMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < newMatrix.GetLength(1); j++)
+                    newMatrix[i, j] /= num;
+            }
+            return newMatrix;
+        }
+    }
+}
