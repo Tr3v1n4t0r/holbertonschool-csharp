f2b9b036529a08c7b7b3a9fdd8d294ade8b82879
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-17 23:58:58 -0800

    t3

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -9,10 +9,26 @@ namespace Text.Tests
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase("abcba")]
+        [TestCase("racecar")]
+        [TestCase("Mom")]
+        [TestCase("a a a    a a   a")]
+        [TestCase("A man, a plan, a canal: Panama.")]
+        [TestCase("Trevorovert")]
+        [TestCase("")]
+        public void Returns_True(string input)
         {
-            Assert.Pass();
+            bool result = Text.Str.IsPalindrome(input);
+            Assert.IsTrue(result);
+        }
+
+        [TestCase("Trevor")]
+        [TestCase("hello world")]
+        [TestCase("racercar")]
+        public void Returns_False(string input)
+        {
+            bool result = Text.Str.IsPalindrome(input);
+            Assert.IsTrue(result);
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,31 @@
 
 namespace Text
 {
-    public class Class1
+    /// <summary>Class for string functions.</summary>
+    public class Str
     {
+        /// <summary>Checks if a string is a palindrome.</summary>
+        /// <param name="s">The string to check.</param>
+        /// <returns>True is palindrome, else false.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            string base = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+                    base += Char.ToLower(c);
+            }
+            int j = 0;
+            int k = base.Length - 1;
+            while (j < k)
+            {
+                if (base[j] != base[k])
+                    return false;
+                j++;
+                k++;
+            }
+            return true;
+        }
     }
 }
