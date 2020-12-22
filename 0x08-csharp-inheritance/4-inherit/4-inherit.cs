a3999c7931c77ba35463738b4d35da2103ad4d06
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2020-12-21 20:55:10 -0800

    t4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,13 @@
+using System;
+
+/// <summary>Empty base Animal class.</summary>
+class Animal
+{
+
+}
+
+/// <summary>Empty dog class that inherits from Animal.</summary>
+class Dog : Animal
+{
+    
+}
