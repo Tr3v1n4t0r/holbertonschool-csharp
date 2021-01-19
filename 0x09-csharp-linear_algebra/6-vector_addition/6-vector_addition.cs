e2057af936b26eb1e27dc6b269ee166710a27b70
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-19 10:00:27 -0800

    incorrect number

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -9,7 +9,7 @@ class VectorMath
     /// <returns>The sum of the vector.</returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        if (vector1.Length != vector2.Length || vector1.Length < 3 || vector1.Length > 3)
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3)
             return new double[1] {-1};
         
         double[] sum = new double[vector1.Length];
