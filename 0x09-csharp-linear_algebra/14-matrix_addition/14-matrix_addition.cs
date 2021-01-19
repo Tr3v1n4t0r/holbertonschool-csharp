f355fcf2e0e7f843e2a204e62f676556bf67329c
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-01-19 10:57:17 -0800

    t14

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,29 @@
+using System;
+
+/// <summary>Contains methods for matrix math.</summary>
+class MatrixMath
+{
+    /// <summary>Gets the sum of 2 matrices.</summary>
+    /// <param name="matrix1">A matrix to add</param>
+    /// <param name="matrix2">A matrix to add</param>
+    /// <returns>The sum of the matrices.</returns>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.Rank != 2 || matrix2.Rank != 2 ||
+            matrix1.GetLength(0) != matrix2.GetLength(0) ||
+            matrix1.GetLength(1) != matrix2.GetLength(1) ||
+            matrix1.GetLength(0) != matrix1.GetLength(1) ||
+            matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3)
+            return new double[1,1] { {-1} };
+
+        double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+
+        for (uint i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (uint j = 0; j < matrix1.GetLength(1); j++)
+                sum[i; j] = Math.Round(matrix1[i, j] + matrix2[i, j], 2)
+        }
+
+        return sum;
+    }
+}
