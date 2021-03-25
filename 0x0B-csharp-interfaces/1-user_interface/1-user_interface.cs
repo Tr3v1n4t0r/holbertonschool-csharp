7364073ade2222f8b9624d7de5c77c75d5805a39
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-03-25 15:02:14 -0700

    this was working better

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -3,27 +3,28 @@ using System;
 /// <summary>A base class for all game objects.</summary>
 abstract class Base
 {
-    public string name = "";
+    public string name { get; set; }
 
-    /// <summary>Gets the string representation of the instance.</summary>
-    /// <returns>The string representation.</returns>
     public override string ToString()
     {
-        return String.Format("{0} is a {1}", name, this.GetType().Name);
+        return $"{name} is a {this.GetType(). Name}";
     }
 }
 
+/// <summary>Defines the properties of an interactive object.</summary>
 interface IInteractive
 {
     void Interact();
 }
 
+/// <summary>Defines the properties of breakable objects.</summary>
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
 
+/// <summary>Defines the properties of collectable objects.</summary>
 interface ICollectable
 {
     bool isCollected { get; set; }
@@ -36,9 +37,18 @@ class TestObject : Base, IInteractive, IBreakable, ICollectable
     public int durability { get; set; }
     public bool isCollected { get; set; }
 
-    public void Interact() { }
+    public void Interact()
+    {
 
-        public void Break() { }
+    }
 
-        public void Collect() { }
+    public void Break()
+    {
+
+    }
+
+    public void Collect()
+    {
+
+    }
 }
