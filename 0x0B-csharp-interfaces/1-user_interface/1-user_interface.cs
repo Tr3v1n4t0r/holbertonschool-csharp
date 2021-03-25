b0d6a7477f9f52653585b75203bd25f4342b017b
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-03-25 14:35:27 -0700

    t1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,48 @@
+using System;
+
+/// <summary>A base class for all game objects.</summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType(). Name}";
+    }
+}
+
+/// <summary>Defines the properties of an interactive object.</summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>Defines the properties of breakable objects.</summary>
+interface IBreakable
+{
+    int durability { get; set; }
+}
+
+/// <summary>Defines the properties of collectable objects.</summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>A test object for inheriting interfaces.</summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+
+    }
+
+    public void Collect()
+    {
+        
+    }
+}
