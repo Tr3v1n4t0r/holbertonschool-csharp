f3e5f7b095071c398a3162cbb4661ac7ca08c8ef
Tr3v1n4t0r <51677330+Tr3v1n4t0r@users.noreply.github.com>
2021-03-26 20:32:08 -0700

    added more documentation

--- 0x0B-csharp-interfaces/2-doors/2-doors.cs
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -34,11 +34,14 @@ interface ICollectable
 /// <summary>A door class to define doors.</summary>
 class Door: Base, IInteractive
 {
+    /// <summary>Constructor for a Door.</summary>
+    /// <param name="name">Name of door, defaulted to "Door"</param>
     public Door(string name = "Door")
     {
         this.name = name;
     }
 
+    /// <summary>Output of interacting with a door.</summary>
     public void Interact()
     {
         Console.WriteLine($"You try to open the {this.name}. It's locked.");
